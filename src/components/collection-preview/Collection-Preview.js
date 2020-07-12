8bfccd33be5f48b8e9358f22e1ed03a40da0de4b
n1ckk007 <antonellos101@gmail.com>
2020-07-12 20:52:51 +1000

    updating app w shop page, collection components

---
+++ src/components/collection-preview/Collection-Preview.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Collection-Preview.scss";
+import CollectionItem from "../collection-item/Collection-item";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        //filter to only show 4 items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;

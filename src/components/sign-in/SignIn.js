975d9c2ac48a4aec62bffd40ed52a63a20a139a2
n1ckk007 <antonellos101@gmail.com>
2020-07-18 17:20:47 +1000

    fixed google sign in error

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -58,7 +58,11 @@ export default class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
